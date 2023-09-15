42c8f446302d790d792ffeb0eec0e9b339d6481d
Anilkokkul <anilkokkul1997@gmail.com>
2023-09-15 21:51:01 +0530

    updated

--- src/components/ForgortPassword.js
+++ src/components/ForgortPassword.js
@@ -7,11 +7,9 @@ import { instance } from "../App";
 const ForgortPassword = () => {
   const [email, setEmail] = useState("");
 
-  const handleSubmit = async () => {
+  const handleSubmit = () => {
     instance
-      .post("/forgot-password", {
-        email,
-      })
+      .post("/forgot-password", email)
       .then((response) => {
         const data = response.data.message;
         toast.success(data, {
